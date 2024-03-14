0d533afe7a7ffbb6dc370103ac3dd0d2baa5bbd8
vkinsella <vivion.kinsella@atu.ie>
2024-03-14 14:52:03 +0000

    Add project files.

---
+++ MyDevOpsProject.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace MyDevOpsProject.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+      
+       
+            [Test]
+            public static void Add_twopositive()
+            {
+                // arrange
+
+                Calculator c1 = new Calculator();
+                // act
+                int result = c1.Add(2, 3);
+                //Assert
+
+                Assert.That(result, Is.EqualTo(5));
+            }
+
+
+            [Test]
+
+            public static void Divide_Zero()
+            {
+                // Arrange
+                Calculator c1 = new Calculator();
+
+                // Act and Assert combined
+                Assert.That(() => c1.Divide(2, 0), Throws.TypeOf<DivideByZeroException>());
+
+            }
+            [Test]
+            public static void FillArray_ExpectedTrue()
+            {
+
+                Calculator c1 = new Calculator();
+                int[] x = new int[6];
+                c1.FillArray(x);
+                Assert.That(x, Is.EqualTo(new int[] { 8, 9, 9, 9, 9, 9 }));
+            }
+        }
+}
---
+++ MyDevOpsProject/Calculator.cs
@@ -0,0 +1,31 @@
+﻿namespace MyDevOpsProject
+{
+    public class Calculator
+    {
+
+        public int Add(int n1, int n2)
+        {
+
+            return n1 + n2;
+        }
+
+
+
+        public int Divide(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new DivideByZeroException("Cannot divide by zero.");
+            else
+                return numerator / denominator;
+        }
+
+        public void FillArray(int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = 9;
+            }
+
+        }
+    }
+}
